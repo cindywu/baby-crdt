971f408fff78b95db540f043d66c96932a8e134a
cindy <1177031+cindywu@users.noreply.github.com>
2023-01-19 10:01:28 -1000

    getting there

--- components/client.tsx
+++ components/client.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { nanoid } from 'nanoid'
 
 type ClientProps = {
   color: string
+  txs: any
+  setTxs: any
+  client: string
 }
 
-export default function Client({ color } : ClientProps) {
+export default function Client({ color, txs, setTxs, client } : ClientProps) {
+  console.log({txs})
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    txs.length !== 0 && setValue(txs[txs.length - 1].value)
+  }, [txs])
+
+  function handleChange(e : any){
+    console.log({e})
+    const tx = {
+      id: nanoid(),
+      client,
+      value: e.target.value
+    }
+    console.log({tx})
+    setTxs([...txs, tx])
+  }
   return (
     <div className={`w-2/5 text-2xl`}>
       <div className={`border-4 ${color}`}>
         <div className={"font-mono text-xs text-zinc-400 p-4"}>Client</div>
         <input
+          onChange={(e) => handleChange(e)}
           className={"w-full p-4 outline-none"}
           placeholder="say something"
+          value={value}
         />
       </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Client from '../components/client'
 import Transactions from '../components/transactions'
 
 export default function Home() {
+  const [txs, setTxs] = useState([])
+
   return (
     <div className={"w-screen"}>
       <div className={"flex justify-center py-10 text-2xl font-bold"}>baby crdt</div>
       <div className={"flex flex-row justify-center space-x-4"}>
         <Client
           color={"border-lime-400"}
+          txs={txs}
+          setTxs={setTxs}
+          client={"clientA"}
         />
         <Client
           color={"border-amber-400"}
+          txs={txs}
+          setTxs={setTxs}
+          client={"clientB"}
         />
       </div>
       <Transactions/>
