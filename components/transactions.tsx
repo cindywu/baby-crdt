223ec61a474343c24af9cbba714a993ff72e9c8d
cindy <1177031+cindywu@users.noreply.github.com>
2023-01-24 12:58:06 -1000

    mobile styles

--- components/transactions.tsx
+++ components/transactions.tsx
@@ -5,12 +5,14 @@ type TransactionsProps = {
 }
 
 export default function Transactions({ txs } : TransactionsProps) {
+  const txsReversed = txs.slice().reverse()
+
   return (
-    <div className={"flex flex-col justify-center pt-20 mx-24"}>
-      <div className={"w-full text-xl p-4 "}>
+    <div className={"flex flex-col justify-center pt-20 mx-0 sm:mx-24"}>
+      <div className={"text-xl p-2 sm:p-0"}>
         <div className={"font-mono text-xs text-zinc-400 pb-4"}>Transactions</div>
-        <div className={"overflow-auto max-h-48"}>
-        {txs && txs.map((tx: any, i: any) => {
+        <div className={"overflow-auto max-h-60 w-screen sm:w-full"}>
+        {txsReversed && txsReversed.map((tx: any, i: any) => {
         return (
           <Transaction
             key={tx.id}
@@ -27,7 +29,7 @@ export default function Transactions({ txs } : TransactionsProps) {
 
 function Transaction({ tx, i} : any) {
   return (
-    <div className={i%2 === 1 ? "grid grid-cols-5 text-xs p-4" : "grid grid-cols-5 text-xs p-4 bg-zinc-200"}>
+    <div className={i%2 === 1 ? "grid grid-cols-5 text-xs p-4 " : "grid grid-cols-5 text-xs p-4 bg-zinc-200"}>
       <div className={"font-mono"}>{tx.id.slice(-6)}</div>
       <div>{tx.client}</div>
       <div>{tx.value}</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className={"w-screen"}>
       <div className={"flex justify-center pt-10 pb-5 text-2xl font-bold"}>baby crdt</div>
       <div className={"flex justify-center pb-5 font-mono text-xs text-zinc-400"}><Link href="https://github.com/cindywu/baby-crdt">github.com/cindywu/baby-crdt</Link></div>
-      <div className={"flex flex-row justify-center space-x-14"}>
+      <div className={"flex flex-row justify-center space-x-2 pl-2 sm:pl-0 sm:space-x-14"}>
         <Client
           color={"border-lime-400"}
           txs={txs}
