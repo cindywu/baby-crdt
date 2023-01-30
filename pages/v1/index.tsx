68324519f081f7562bca86e6eb058e2acdb59676
cindy <1177031+cindywu@users.noreply.github.com>
2023-01-30 10:26:00 -1000

    create /v2 and snapshot baby-crdt

--- pages/v1/index.tsx
+++ pages/v1/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className={"w-screen"}>
-      <div className={"flex justify-center py-10 text-2xl font-bold"}>baby crdt</div>
+      <div className={"flex justify-center py-10 text-2xl font-bold"}>baby crdt v1</div>
       <div className={"flex flex-row justify-center space-x-14"}>
         <Client
           color={"border-lime-400"}
---
+++ pages/v2/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Client from '../../components/client-v2'
+import Transactions from '../../components/transactions-v2'
+import Link from 'next/link'
+
+export default function Home() {
+  const [txs, setTxs] = useState<any[]>([])
+
+  return (
+    <div className={"w-screen"}>
+      <div className={"flex justify-center pt-10 pb-5 text-2xl font-bold"}>baby crdt v2</div>
+      <div className={"flex justify-center pb-5 font-mono text-xs text-zinc-400"}><Link href="https://github.com/cindywu/baby-crdt">github.com/cindywu/baby-crdt</Link></div>
+      <div className={"flex flex-row justify-center space-x-2 pl-2 sm:pl-0 sm:space-x-14"}>
+        <Client
+          color={"border-lime-400"}
+          txs={txs}
+          setTxs={setTxs}
+          client={"Client A"}
+        />
+        <Client
+          color={"border-amber-400"}
+          txs={txs}
+          setTxs={setTxs}
+          client={"Client B"}
+        />
+      </div>
+      <Transactions
+        txs={txs}
+      />
+      <div className={"fixed bottom-0 font-mono text-xs text-zinc-400 w-screen text-center py-8 bg-white"}>
+      <Link href="https://cindy-wu.com">cindy-wu.com</Link>
+      </div>
+    </div>
+  )
+}
