9a45954f937a62b4fde6bcd144648980cd8e6559
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-03 07:00:09 -1000

    styles are broken but the app is working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 
-import { AppProps } from 'next/app'
+import React from 'react'
 import '../styles/global.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function RootLayout({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
+  )
 }
